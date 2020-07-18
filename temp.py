0358d51a4cba4bc4d25beb0835048394a614135e
akbar <akkuakbar97@gmail.com>
2020-07-19 00:42:13 +0530

    new commit

--- temp.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import pandas as pd
-import csv
-import time
-driver = webdriver.Chrome("/home/akbar/PROJECTS/webscrapping/chromedriver")
